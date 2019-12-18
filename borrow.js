a0ebb704772a2257d04e44cc559a278aea07c823
Zachary <zachjoliver@gmail.com>
2019-12-17 23:08:44 -0800

    Add regex solution

--- borrow.js
+++ borrow.js
@@ -1,13 +1,17 @@
 function borrow(s) {
-  let alpha = 'abcdefghijklmnopqrstuvwxyz';
-  let retStr = "";
-
-  for (let i = 0; i < s.length; i++) {
-    let char = s[i].toLowerCase();
-    if (alpha.includes(char)) {
-      retStr += char;
-    }
-  }
-
-  return retStr;
+  return s.toLowerCase().replace(/\W/g,'');
 }
+
+// function borrow(s) {
+//   let alpha = 'abcdefghijklmnopqrstuvwxyz';
+//   let retStr = "";
+
+//   for (let i = 0; i < s.length; i++) {
+//     let char = s[i].toLowerCase();
+//     if (alpha.includes(char)) {
+//       retStr += char;
+//     }
+//   }
+
+//   return retStr;
+// }
